1be494a8051d2f6b538df1969343cd0a67d3530b
klambert <klambert@v3.cluster.local>
2025-05-09 16:33:57 -0400

    testing out changes

--- train_klamb.sh
+++ train_klamb.sh
@@ -7,7 +7,7 @@
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=4                                                                     
 #SBATCH --mem=16GB                                                  
-#SBATCH --time=06:00:00
+#SBATCH --time=06:00:00 
 
 # Print the name of the node and job ID for debugging
 echo "Running on node: $(hostname)"
